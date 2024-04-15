ec2b270bd4c84e6b64af92f361874cc3b121d15c
do-develop <83389587+do-develop@users.noreply.github.com>
2024-04-15 16:17:17 +0200

    fix: remove api key

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
     production: false,
     weatherApiBaseUrl: 'https://api.open-meteo.com/v1/metno',
     geolocationApiBaseUrl: 'https://maps.googleapis.com/maps/api/geocode/json',
-    geolocationAPIKey: 'AIzaSyAgsyS5qbdicDh5tSwC3cQiOj_2fEIbTkk'
+    geolocationAPIKey: ''
 };
